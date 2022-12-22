e10c745150cf6759a17ed6cb1360f0d6f2d5e0d7
Carlos Agosto <carlos.agosto@gmail.com>
2022-12-22 22:10:16 +0000

    flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { createTheme, ThemeProvider } from '@mui/material';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import FlexBoxLayouts from './layouts';
 
 function App() {
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+
+        lg: 1200,
+        xl: 1600,
+      },
+    },
+  });
   return (
-    <Router>
-      <Routes>
-        <Route index element={<FlexBoxLayouts />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route index element={<FlexBoxLayouts />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,52 @@
-import { Stack } from '@mui/material';
+import { Box, Stack, Theme, useMediaQuery } from '@mui/material';
+import { faker } from '@faker-js/faker';
+import { times } from 'lodash';
 
 export default function FlexBoxLayouts() {
-  return <Stack>hi</Stack>;
+  const boxes = times(10);
+  const mdBP = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'));
+
+  return (
+    <Box>
+      <Stack
+        direction="row"
+        columnGap={2}
+        rowGap={1}
+        flexWrap="wrap"
+        sx={{ p: 2 }}
+      >
+        {boxes.map((v) => (
+          <Box key={v}>{faker.name.fullName()}</Box>
+        ))}
+      </Stack>
+      <Stack direction={!mdBP ? 'column' : 'row'} columnGap={2} sx={{ p: 3 }}>
+        <Box
+          sx={{
+            background: (theme) => theme.palette.primary.main,
+            p: 10,
+            flex: 1,
+            order: {
+              sm: 1,
+              md: 0,
+            },
+          }}
+        />
+        <Box
+          sx={{
+            background: (theme) => theme.palette.secondary.dark,
+            p: 10,
+            flexBasis: {
+              xs: 1,
+              md: 150,
+            },
+            order: {
+              sm: 0,
+              md: 1,
+            },
+          }}
+        />
+      </Stack>
+    </Box>
+  );
 }
+
